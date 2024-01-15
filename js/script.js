d265b80f991a88197591801946cffbb43dbd6b47
Nans PHILIP <nansphilip@gmail.com>
2024-01-15 17:19:26 +0100

    Lourd

--- js/script.js
+++ js/script.js
@@ -1,81 +1,82 @@
-let $paragraphEl = $('.paragraph');
-let $textEl = $('.paragraph > .text');
-let $buttonElList = $('.paragraph > .button');
-let $textMaxHeight, $textMinHeight = undefined;
-let $minimizeSate = true;
-
-let IsVisible = () => {
-
-    // Store minimized state if all text isn't visible
-    if (!$paragraphEl.hasClass('visible')) {
-        $minimizeSate = $paragraphEl.hasClass('minimized');
-    }
-
-    // Reset
-    $textEl.css('height', '');
-    $paragraphEl.removeClass('visible');
-    $paragraphEl.removeClass('minimized');
-
-    // Mesure number of lines
-    let $paragraphHeight = $textEl.height();
-    let $lineHeight = parseInt($textEl.css('line-height'));
-    let $numberOfLines = $paragraphHeight / $lineHeight;
-
-    // Set class
-    if ($numberOfLines <= 3) {
-        $paragraphEl.addClass('visible');
-        return true;
-    } else {
-        if ($minimizeSate) {
-            $paragraphEl.addClass('minimized');
-        }
-        return false;
-    }
-};
-
-let MesureHeights = () => {
-    if ($paragraphEl.hasClass('minimized')) {
-        $textMinHeight = $textEl.css('height');
-        $paragraphEl.toggleClass('minimized');
-        $textMaxHeight = $textEl.css('height');
-    }
-    else {
-        $textMaxHeight = $textEl.css('height');
-        $paragraphEl.toggleClass('minimized');
-        $textMinHeight = $textEl.css('height');
-    }
-};
-
-let ToggleParagraph = () => {
-    $paragraphEl.toggleClass('minimized');
-
-    if ($paragraphEl.hasClass('minimized')) {
-        $textEl.css('height', $textMinHeight);
-    }
-    else {
-        $textEl.css('height', $textMaxHeight);
-    }
-};
-
 $(document).ready(() => {
+    $('.paragraph').each(function() {
+        let $paragraphEl = $(this);
+        let $textEl = $paragraphEl.find('.text');
+        let $buttonElList = $paragraphEl.find('.button');
+        let $textMaxHeight, $textMinHeight = undefined;
+        let $minimizeSate = true;
 
-    // On load
-    if (!IsVisible()) {
-        MesureHeights();
-        ToggleParagraph();
-    }
+        let IsVisible = () => {
 
-    // On resize
-    $(window).resize(() => {
+            // Store minimized state if all text isn't visible
+            if (!$paragraphEl.hasClass('visible')) {
+                $minimizeSate = $paragraphEl.hasClass('minimized');
+            }
+
+            // Reset
+            $textEl.css('height', '');
+            $paragraphEl.removeClass('visible');
+            $paragraphEl.removeClass('minimized');
+
+            // Mesure number of lines
+            let $paragraphHeight = $textEl.height();
+            let $lineHeight = parseInt($textEl.css('line-height'));
+            let $numberOfLines = $paragraphHeight / $lineHeight;
+
+            // Set class
+            if ($numberOfLines <= 3) {
+                $paragraphEl.addClass('visible');
+                return true;
+            } else {
+                if ($minimizeSate) {
+                    $paragraphEl.addClass('minimized');
+                }
+                return false;
+            }
+        };
+
+        let MesureHeights = () => {
+            if ($paragraphEl.hasClass('minimized')) {
+                $textMinHeight = $textEl.css('height');
+                $paragraphEl.toggleClass('minimized');
+                $textMaxHeight = $textEl.css('height');
+            }
+            else {
+                $textMaxHeight = $textEl.css('height');
+                $paragraphEl.toggleClass('minimized');
+                $textMinHeight = $textEl.css('height');
+            }
+        };
+
+        let ToggleParagraph = () => {
+            $paragraphEl.toggleClass('minimized');
+
+            if ($paragraphEl.hasClass('minimized')) {
+                $textEl.css('height', $textMinHeight);
+            }
+            else {
+                $textEl.css('height', $textMaxHeight);
+            }
+        };
+
+
+        // On load
         if (!IsVisible()) {
             MesureHeights();
             ToggleParagraph();
         }
-    });
 
-    // On click
-    $buttonElList.on('click', () => {
-        ToggleParagraph();
+        // On resize
+        $(window).resize(() => {
+            if (!IsVisible()) {
+                MesureHeights();
+                ToggleParagraph();
+            }
+        });
+
+        // On click
+        $buttonElList.on('click', () => {
+            ToggleParagraph();
+        });
     });
 });
-
